78174e4ad59d0755031658adb719a08b22137116
k-two-000002545d <g0115089s7@gmail.com>
2024-06-18 18:54:29 +0900

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: "export",
+    basePath: 'https://github.com/k-two-000002545d/k-two-000002545d.github.io.git',
+};
+
+export default nextConfig;
