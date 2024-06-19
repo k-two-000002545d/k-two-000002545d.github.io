6d3d57da69a32ef6f465bd896d740a10b7fe6194
k-two-000002545d <g0115089s7@gmail.com>
2024-06-19 20:06:38 +0900

    3回目更新

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
           <div className="md:w-1/2 lg:max-w-lg w-5/6 mb-16 lg:pr-24 md:pr-16">
             <img className="relative inline-block rounded-full overflow-hidden" src="./img/icon.png" alt="" />
           </div>
-          <div className="md:w-1/2 flex-grow">
+          <div className="md:w-1/2 flex-grow text-center md:text-left">
             <h1 className="text-3xl md:text-6xl text-gray-900 font-medium mb-4">
               K-TWO
               <br />
@@ -20,7 +20,76 @@ export default function Home() {
               JavaScript and Python.
             </h1>
             <p className="mb-8 leading-relaxed">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab rerum est debitis, ullam ducimus officiis optio assumenda iure et labore eaque quidem animi quam, impedit laborum. Eligendi nam tempore repellat!</p>
-            <button>Contact</button>
+            <button className="justify-center px-6 py-2 border-0 rounded-md text-lg text-white bg-sky-500 hover:bg-sky-600 duration-200">Contact</button>
+          </div>
+        </div>
+      </section>
+
+      <section className="text-gray-700 border-t border-gray-200">
+        <div className="container px-5 py-12 w-full mx-auto">
+          <div className="text-center mb-12">
+            <h1 className="text-2xl sm:text-3xl font-medium mb-2 text-gray-900">Skils</h1>
+            <p className=" pb-1">
+              PythonとJavaScriptをメインに自動化や業務効率化システムを組むことが多いです。
+              <br />
+              最近はStable Diffusionで独自Loraを作成して画像生成するのにハマっています。
+            </p>
+          </div>
+          <div className="flex flex-wrap">
+            <div className="md:w-1/4 p-3">
+              <div className="bg-gray-50 rounded-lg p-4 border">
+                <div className="flex items-center justify-center">
+                  <div className="rounded-full">
+                    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 48 48">
+                      <path fill="#0277BD" d="M24.047,5c-1.555,0.005-2.633,0.142-3.936,0.367c-3.848,0.67-4.549,2.077-4.549,4.67V14h9v2H15.22h-4.35c-2.636,0-4.943,1.242-5.674,4.219c-0.826,3.417-0.863,5.557,0,9.125C5.851,32.005,7.294,34,9.931,34h3.632v-5.104c0-2.966,2.686-5.896,5.764-5.896h7.236c2.523,0,5-1.862,5-4.377v-8.586c0-2.439-1.759-4.263-4.218-4.672C27.406,5.359,25.589,4.994,24.047,5z M19.063,9c0.821,0,1.5,0.677,1.5,1.502c0,0.833-0.679,1.498-1.5,1.498c-0.837,0-1.5-0.664-1.5-1.498C17.563,9.68,18.226,9,19.063,9z"></path><path fill="#FFC107" d="M23.078,43c1.555-0.005,2.633-0.142,3.936-0.367c3.848-0.67,4.549-2.077,4.549-4.67V34h-9v-2h9.343h4.35c2.636,0,4.943-1.242,5.674-4.219c0.826-3.417,0.863-5.557,0-9.125C41.274,15.995,39.831,14,37.194,14h-3.632v5.104c0,2.966-2.686,5.896-5.764,5.896h-7.236c-2.523,0-5,1.862-5,4.377v8.586c0,2.439,1.759,4.263,4.218,4.672C19.719,42.641,21.536,43.006,23.078,43z M28.063,39c-0.821,0-1.5-0.677-1.5-1.502c0-0.833,0.679-1.498,1.5-1.498c0.837,0,1.5,0.664,1.5,1.498C29.563,38.32,28.899,39,28.063,39z"></path>
+                    </svg>
+                  </div>
+                  <h2 className="ml-2">
+                    Python
+                  </h2>
+                </div>
+              </div>
+            </div>
+            <div className="md:w-1/4 p-3">
+              <div className="bg-gray-50 rounded-lg p-4 border">
+                <div className="flex items-center justify-center">
+                  <div className="rounded-full">
+                  <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 48 48">
+                    <path fill="#ffd600" d="M6,42V6h36v36H6z"></path><path fill="#000001" d="M29.538 32.947c.692 1.124 1.444 2.201 3.037 2.201 1.338 0 2.04-.665 2.04-1.585 0-1.101-.726-1.492-2.198-2.133l-.807-.344c-2.329-.988-3.878-2.226-3.878-4.841 0-2.41 1.845-4.244 4.728-4.244 2.053 0 3.528.711 4.592 2.573l-2.514 1.607c-.553-.988-1.151-1.377-2.078-1.377-.946 0-1.545.597-1.545 1.377 0 .964.6 1.354 1.985 1.951l.807.344C36.452 29.645 38 30.839 38 33.523 38 36.415 35.716 38 32.65 38c-2.999 0-4.702-1.505-5.65-3.368L29.538 32.947zM17.952 33.029c.506.906 1.275 1.603 2.381 1.603 1.058 0 1.667-.418 1.667-2.043V22h3.333v11.101c0 3.367-1.953 4.899-4.805 4.899-2.577 0-4.437-1.746-5.195-3.368L17.952 33.029z"></path>
+                  </svg>
+                  </div>
+                  <h2 className="ml-2">
+                    JavaScript
+                  </h2>
+                </div>
+              </div>
+            </div>
+            <div className="md:w-1/4 p-3">
+              <div className="bg-gray-50 rounded-lg p-4 border">
+                <div className="flex items-center justify-center">
+                  <div className="rounded-full">
+                    
+                  </div>
+                  <h2 className="ml-2">
+                    Python
+                  </h2>
+                </div>
+              </div>
+            </div>
+            <div className="md:w-1/4 p-3">
+              <div className="bg-gray-50 rounded-lg p-4 border">
+                <div className="flex items-center justify-center">
+                  <div className="rounded-full">
+                    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 48 48">
+                      <path fill="#0277BD" d="M24.047,5c-1.555,0.005-2.633,0.142-3.936,0.367c-3.848,0.67-4.549,2.077-4.549,4.67V14h9v2H15.22h-4.35c-2.636,0-4.943,1.242-5.674,4.219c-0.826,3.417-0.863,5.557,0,9.125C5.851,32.005,7.294,34,9.931,34h3.632v-5.104c0-2.966,2.686-5.896,5.764-5.896h7.236c2.523,0,5-1.862,5-4.377v-8.586c0-2.439-1.759-4.263-4.218-4.672C27.406,5.359,25.589,4.994,24.047,5z M19.063,9c0.821,0,1.5,0.677,1.5,1.502c0,0.833-0.679,1.498-1.5,1.498c-0.837,0-1.5-0.664-1.5-1.498C17.563,9.68,18.226,9,19.063,9z"></path><path fill="#FFC107" d="M23.078,43c1.555-0.005,2.633-0.142,3.936-0.367c3.848-0.67,4.549-2.077,4.549-4.67V34h-9v-2h9.343h4.35c2.636,0,4.943-1.242,5.674-4.219c0.826-3.417,0.863-5.557,0-9.125C41.274,15.995,39.831,14,37.194,14h-3.632v5.104c0,2.966-2.686,5.896-5.764,5.896h-7.236c-2.523,0-5,1.862-5,4.377v8.586c0,2.439,1.759,4.263,4.218,4.672C19.719,42.641,21.536,43.006,23.078,43z M28.063,39c-0.821,0-1.5-0.677-1.5-1.502c0-0.833,0.679-1.498,1.5-1.498c0.837,0,1.5,0.664,1.5,1.498C29.563,38.32,28.899,39,28.063,39z"></path>
+                    </svg>
+                  </div>
+                  <h2 className="ml-2">
+                    Python
+                  </h2>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </section>
